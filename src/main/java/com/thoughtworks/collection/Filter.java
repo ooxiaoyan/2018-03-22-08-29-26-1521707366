d6c0d517d7efb82315202cbfce85b6edf0319199
ooxiaoyan <810978527@qq.com>
2018-04-05 21:46:36 +0800

    commit Filter.java

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -1,31 +1,31 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
     List<Integer>  array;
 
-    public Filter(List<Integer> array) {
-     this.array = array;
-    }
+    public Filter(List<Integer> array) { this.array = array; }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        List<Integer> resultList = array.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
+        return resultList;
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        List<Integer> resultList = array.stream().filter(num -> num % 3 == 0).collect(Collectors.toList());
+        return resultList;
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        List<Integer> resultList = array.stream().distinct().collect(Collectors.toList());
+        return resultList;
+    }
+
+    public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
+        List<Integer> resultList = firstList.stream().filter(item -> secondList.contains(item)).collect(Collectors.toList());
+        return resultList;
     }
 }
