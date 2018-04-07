46264532c6f183962ea511172684dd9199525872
ooxiaoyan <810978527@qq.com>
2018-04-07 22:36:56 +0800

    commit InverseReduce.java

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -16,6 +15,11 @@ public class InverseReduce {
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        while (number > 2) {
+            number = number -2;
+            resultList.add(number);
+        }
+        return resultList;
     }
 }
---
+++ src/main/java/com/thoughtworks/collection/SingleLinkImpl.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.collection;
+
+/**
+ * Created by 筱湮 on 2018/4/7 0007.
+ */
+public class SingleLinkImpl {
+}
